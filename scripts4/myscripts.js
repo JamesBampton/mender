907152fd7277b6efeb22bf1f6df3ff10257440ed
James Bampton <bamptini@hotmail.com>
2025-06-18 14:42:42 +0100

    initial upload of files to git reporitory

---
+++ scripts4/myscripts.js
@@ -0,0 +1,37 @@
+<script>
+	$(document).ready(function() {
+		$('[data-toggle="tooltip"]').tooltip()
+	});
+</script>
+
+	<script>
+		$(document).ready( function () {
+			$('#actmap').DataTable()
+});
+</script>
+
+	<script>
+		$(document).ready( function () {
+			$('#example').DataTable()
+});
+</script>
+
+	<script>
+		$(document).ready( function () {
+			$('#status').DataTable()
+});
+</script>
+
+	<script>
+		$(document).ready(function(){
+			$("#customerToast").toast('show')
+});
+</script>
+
+	<script type="text/javascript">
+// Popup Google Maps window
+		function newPopup(url) {
+			popupWindow = window.open(
+				url, 'popUpWindow', 'height=500,width=600,left=10,top=10,resizable=yes,scrollbars=yes,toolbar=yes,menubar=no,location=no,directories=no,status=yes')
+		}
+</script>
